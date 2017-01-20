eb571e2ae5c278d6d8817b1d3f7c373d13cafcb5
ktran98 <peterkhanhtran@gmail.com>
2017-01-20 13:55:11 -0600

    Add files via upload

---
+++ power.js
@@ -0,0 +1,28 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0]
+]);
+
+function power(mtrx,p){
+    //mtrx is the matrix you input
+    //p is the power of the matrix you want
+    let old = mtrx;
+    for (A,5);
+    old.prod (A);
+    if old.prod (A);
+    old.prod = A*A;
+    return old.prod;
+
+    if old.prod (A,2);
+    old.prod = (A,2)*A;
+    return old.prod       
+
+}
+
+
+//testing code
+console.log(power(A,10));
